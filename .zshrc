76b704940179646693285c14d7c745625dbcb60a
Ryu Tanabe <r-tanabe@media-inc.co.jp>
2021-11-20 21:57:47 +0900

    first commit

---
+++ .zshrc
@@ -0,0 +1,4 @@
+eval "$(starship init zsh)"
+eval "$(pyenv init -)"
+
+export PATH=$HOME/.nodebrew/current/bin:$PATH
---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dotfiles=(.zshrc .tmux.conf)
+for file in "${dotfiles[@]}"; do
+  ln -svf $file ~/
+done
